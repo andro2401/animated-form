c960c99ef2b021ae124098808191904f7320d14c
Andro <androistuk@gmail.com>
2022-02-28 20:04:01 +0100

    finished form with all animations

---
+++ app.js
@@ -0,0 +1,17 @@
+let tl = new gsap.timeline({ defaults: {ease: Circ.easeOut}}) //to su funkcije koje su predefinirane za animaciju lottia
+let cta = document.querySelector('.cta')
+const player = document.querySelector('.lottie')
+
+
+
+tl.paused(true)
+tl.to('.seq', {y:40, opacity: 0, stagger: .05})
+tl.to('.form-container', {opacity:0}, "-=.4")
+tl.to('h2', {opacity:1, duration: 2, y:20}, "+=1")
+
+cta.addEventListener('click', () => {
+    tl.play()
+    setTimeout(() => {
+        player.play()
+    }, 800)
+})
